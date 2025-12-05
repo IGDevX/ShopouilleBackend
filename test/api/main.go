8b80cfe2410be9f5310da403174390cacb4d2bc2
Mathias <mathias.puyfages@gmail.com>
2025-12-05 17:57:07 +0100

    fix : allow header reading for grafana in the api

--- test/api/main.go
+++ test/api/main.go
@@ -192,6 +192,8 @@ func corsMiddleware(next http.HandlerFunc) http.HandlerFunc {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
+		w.Header().Set("Access-Control-Expose-Headers", "Content-Type, Content-Length")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Max-Age", "3600")
 
 		if r.Method == http.MethodOptions {
